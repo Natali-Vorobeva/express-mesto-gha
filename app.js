6abf1638a3349eb840642e27c6ac10ee91d8904d
Natalia Vorobjeva <vorobjeva.natalia76@yandex.ru>
2023-04-26 14:27:02 +0300

    feat: первые наработки пр14

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const { errors } = require('celebrate');
 const router = require('./routes');
-const login = require('./controllers/users');
-const createUser = require('./controllers/users');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3003 } = process.env;
 
 const app = express();
 mongoose.connect('mongodb://0.0.0.0:27017/mestodb');
@@ -13,10 +12,9 @@ mongoose.connect('mongodb://0.0.0.0:27017/mestodb');
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/signin', login);
-app.post('/signup', createUser);
-
 app.use(router);
+
+app.use(errors());
 app.use((err, req, res, next) => {
   const { statusCode = 500, message } = err;
 
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,30 @@
 const router = require('express').Router();
-const auth = require('../middlewares/auth');
+const { celebrate, Joi } = require('celebrate');
 
+const auth = require('../middlewares/auth');
 const usersRouter = require('./users');
 const cardsRouter = require('./cards');
+const login = require('./users');
+const createUser = require('./users');
 
 const NOT_FOUND_ERROR_CODE = 404;
 
 const notFoundRouter = (req, res) => {
   res.status(NOT_FOUND_ERROR_CODE).send({ message: '404 - Страница не найдена' });
 };
+
+router.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().min(2).max(30),
+    password: Joi.string().required().min(8).max(8),
+  }).unknown(true),
+}), login);
+router.post('/signup', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().min(2).max(30),
+    password: Joi.string().required().min(8).max(8),
+  }).unknown(true),
+}), createUser);
 
 router.use('/users', auth, usersRouter);
 router.use('/cards', auth, cardsRouter);
