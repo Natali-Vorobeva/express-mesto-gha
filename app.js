07471c42dee1a612f3f4fd58319f8c5418ecc682
Natalia Vorobjeva <vorobjeva.natalia76@yandex.ru>
2023-05-19 05:14:38 +0300

    Доработать ошибки в контроллере deleteCard

--- app.js
+++ app.js
@@ -1,27 +1,49 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
+const cors = require('cors');
+const helmet = require('helmet');
+const rateLimit = require('express-rate-limit');
 const routes = require('./routes');
 
-const NOT_FOUND_ERROR_CODE = 404;
+const { requestLogger, errorLogger } = require('./middlewares/logger');
+
 const BAD_REQUEST_ERROR_CODE = 400;
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
 mongoose.connect('mongodb://0.0.0.0:27017/mestodb');
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(limiter);
+
+app.use(cors());
+
+app.use(requestLogger);
+app.use(helmet());
+app.use(errorLogger);
+
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use(routes);
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use((err, req, res, next) => {
-  if (err.name === 'CastError') {
-    res.status(NOT_FOUND_ERROR_CODE).send({ message: '404 — Не найдено' });
-    return;
-  }
   if (err.name === 'ValidationError ') {
     res.status(BAD_REQUEST_ERROR_CODE).send({ message: '400 — Переданы некорректные данные' });
     return;
@@ -31,7 +53,7 @@ app.use((err, req, res, next) => {
     .status(statusCode)
     .send({
       message: statusCode === 500
-        ? 'На сервере произошла ошибка'
+        ? 'На сервере произошла ошибка!!'
         : message,
     });
   next();
